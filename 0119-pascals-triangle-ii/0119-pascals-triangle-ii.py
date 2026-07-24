e61fc919749868fddf18304e1203bfc53f706f09
Soham Thorat <140864496+ThoratSoham@users.noreply.github.com>
2026-07-24 18:42:07 +0530

    Time: 15 ms (1.03%), Space: 12.4 MB (52.29%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        
+        triangle = []
+
+        for i in range(100):
+            row = [1] * (i + 1)
+
+            for j in range(1,i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+            triangle.append(row)
+
+        return triangle[rowIndex]
