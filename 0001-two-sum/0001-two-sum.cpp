5a0fd2cceb678a5494d13e78d531c5671f90579e
Soham Thorat <140864496+ThoratSoham@users.noreply.github.com>
2026-08-19 13:56:11 +0530

    0001-TWO-SUM Stats: Time: 35 ms (35.78%), Space: 14.2 MB (73.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //int arr[2];
         int n = nums.size();
-        for (int i = 0; i<n; i++){
-            for (int j = i+1; j < n; j++)
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
             {
-                if (nums[i]+nums[j] == target)
+                if (nums[i] + nums[j] == target)
                 {
                     return {i,j};
                 }
-
             }
         }
-    return {-1,-1};
+        return {-1,-1};
     }
 };
