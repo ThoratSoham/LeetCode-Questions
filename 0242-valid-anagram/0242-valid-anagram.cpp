300f6778087a4fcad3dc6d38d5da0b5fb08d3d62
Soham Thorat <140864496+ThoratSoham@users.noreply.github.com>
2026-08-25 14:56:16 +0530

    0242-VALID-ANAGRAM Stats: Time: 43 ms (6.81%), Space: 18.8 MB (11.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,22 @@
 #include<unordered_map>
 #include<string>
-using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> charCount1, charCount2;
-        for (char c1 : s){
-            charCount1[c1]++;
+        unordered_map<char,int> char_map1;
+        unordered_map<char,int> char_map2;
+        for (char c : s){
+            if (char_map1.contains(c)){
+                char_map1[c]++;
+            }
+            char_map1.insert(make_pair(c, 1));
         }
-        for (char c2 : t){
-            charCount2[c2]++;
+        for (char d : t){
+            if (char_map2.contains(d)){
+                char_map2[d]++;
+            }
+            char_map2.insert(make_pair(d, 1));
         }
-        return charCount1 == charCount2;
+        return char_map1 == char_map2;
     }
 };
