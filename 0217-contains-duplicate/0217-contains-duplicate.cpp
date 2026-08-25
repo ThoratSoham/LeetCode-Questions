1124bea53ce3632f58d277a2ba7c516cee9bd4e3
Soham Thorat <140864496+ThoratSoham@users.noreply.github.com>
2026-08-25 14:44:15 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 80 ms (24.14%), Space: 111.3 MB (61.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,12 +2,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    unordered_set<int> seen;
-    for (int num : nums){
-        if (seen.count(num)){
-            return true;
-        } seen.insert(num);
-    }
-    return false;
+        unordered_set<int> nums_set;
+
+        for(int num : nums){
+            if (nums_set.count(num) > 0){
+                return true;
+            }
+            nums_set.insert(num);
+        }
+        return false;
     }
 };
