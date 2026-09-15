5a1be7361cb69124fc29cf0160f10447ad66225f
Soham Thorat <140864496+ThoratSoham@users.noreply.github.com>
2026-09-15 20:42:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        // code here
+        for(int i = 0; i<arr.size(); i++){
+            for(int j = i+1; j<arr.size(); j++){
+                if (arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+};
